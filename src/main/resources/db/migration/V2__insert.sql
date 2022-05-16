26fadeb064add180ad657d2f59a7be389cbff90a
GrushinDmitry <dmitriyrzn1991@gmail.com>
2022-05-16 18:55:52 +0300

    init 1

---
+++ src/main/resources/db/migration/V2__insert.sql
@@ -0,0 +1,22 @@
+insert into CATEGORY (name)
+values  ('Food'),
+        ('Auto'),
+        ('Household chemical'),
+        ('Help'),
+        ('Presents'),
+        ('Household appliances'),
+        ('Babies'),
+        ('Pets'),
+        ('Health and beauty'),
+        ('Credits and debts'),
+        ('Invoice'),
+        ('Tax and insurance'),
+        ('Leisure'),
+        ('Education'),
+        ('Сlothes and accessories'),
+        ('Renovation and furniture'),
+        ('Transport'),
+        ('Business'),
+        ('Rest'),
+        ('Investments');
+
